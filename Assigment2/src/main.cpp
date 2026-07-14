4d881dff43b715d2bc90a25785fd0b0e31467645
Ren4 <pro100ren4@gmail.com>
2026-07-15 02:48:34 +0300

    Проверка менеджера сущностей

--- Assigment2/src/main.cpp
+++ Assigment2/src/main.cpp
@@ -18,5 +18,33 @@ int main(void)
 	Game game(GetApplicationDirectory());
 	game.run();
 
+  // Testing enitity manager
+#if 0 
+  int score = 0;
+  const int maxScore = 32;
+  for (int i = 0; i < 100; i++)
+  {
+    std::shared_ptr<Entity> it = manager.addEntity("Test");
+    it->score = std::make_shared<CScore>(score++);
+    score = score % maxScore;
+  }
+  
+  manager.update();
+  
+  EntitiesVector entities = manager.getEntities();
+
+  for (int i = 0; i < 100; i++)
+  {
+    if (entities[i]->score->score % 4 == 0)
+      entities[i]->destroy();
+  }
+
+  EntitiesVector entitiesBeforeUpdated = manager.getEntities();
+
+  manager.update();
+
+  EntitiesVector entitiesUpdated = manager.getEntities();
+#endif
+
 	return 0;
 }
